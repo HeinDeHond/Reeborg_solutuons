429e5d415d04e27abd1148ccddbe8f91743155e4
HeinDeHond <113357850+HeinDeHond@users.noreply.github.com>
2023-05-01 23:14:59 +0200

    Update Center.py

--- Center.py
+++ Center.py
@@ -16,3 +16,37 @@ def walk():
         done()
     
 walk()
+
+# Center 2 
+think(10)
+def turn_around(): 
+    for i in range(2): 
+        turn_left()
+
+def turn_right():
+    for i in range(3):
+        turn_left()
+
+def walk(): 
+    while not object_here() and front_is_clear(): 
+        put('token')
+        move()
+        if wall_in_front():
+            turn_left()
+            if is_facing_north() and front_is_clear():
+                put('token')
+                move()
+                turn_left()  
+            elif front_is_clear(): 
+                put('token')
+                turn_left()
+                turn_left()
+                move()
+                turn_right()
+def trace_back(): 
+    put('token') 
+    turn_left()
+    
+
+walk()
+            
