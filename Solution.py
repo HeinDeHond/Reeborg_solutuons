cce32225a0ec0fcc53f68493718909b76d377de2
HeinDeHond <113357850+HeinDeHond@users.noreply.github.com>
2023-05-01 10:19:24 +0200

    Create Solution.py

---
+++ Solution.py
@@ -0,0 +1,40 @@
+# HW 1
+move()
+move()
+
+# HW2 
+move()
+move()
+
+#HW4 
+def move_3(): 
+    while front_is_clear(): 
+        move()
+
+def turn_right(): 
+    for i in range(3): 
+        turn_left()
+        
+move_3()
+turn_left()
+move_3()
+turn_right()
+move()
+turn_right()
+move_3()
+turn_left()
+move_3()
+turn_right()
+move()
+turn_right()
+move_3()
+turn_left()
+move_3()
+turn_right()
+move() 
+turn_right()
+move_3()
+turn_left()
+move_3()
+    
+           
